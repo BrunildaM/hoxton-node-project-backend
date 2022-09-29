99bff7010fcd0858a4c68e54e7231984a6b27885
Brunilda Muco <brunildamuco6@gmail.com>
2022-09-29 11:28:50 +0200

    delete

--- src/server.ts
+++ src/server.ts
@@ -19,12 +19,11 @@ function generateToken(id: number) {
   return jwt.sign({ id: id }, SECRET, { expiresIn: "30d" });
 }
 
-
-
+//Getting all the users and their messages includng the rooms
 app.get("/users", async (req, res) => {
   try {
     const users = await prisma.user.findMany({
-      include: { messages: true },
+      include: { messages: true, rooms: true, participants: true },
     });
     res.send(users);
   } catch (error) {
@@ -33,12 +32,13 @@ app.get("/users", async (req, res) => {
   }
 });
 
+//getting a single user
 app.get("/user/:id", async (req, res) => {
   try {
     const id = Number(req.params.id);
     const user = await prisma.user.findUnique({
       where: { id },
-      include: { messages: true, rooms: true },
+      include: { messages: true, rooms: true, participants: true },
     });
     if (user) {
       res.send(user);
@@ -51,38 +51,32 @@ app.get("/user/:id", async (req, res) => {
   }
 });
 
-
-
-async function getCurrentUser (token: string) {
-  const decodedData = jwt.verify(token, SECRET)
+async function getCurrentUser(token: string) {
+  const decodedData = jwt.verify(token, SECRET);
   const user = await prisma.user.findUnique({
     // @ts-ignore
     where: { id: decodedData.id },
-    include: { messages: true,  rooms: true, participants: true }
-  })
-  return user
+    include: { messages: true, rooms: true, participants: true },
+  });
+  return user;
 }
 
 // res.send({ user: user, token: generateToken(user.id) })
 
-
-
-
-app.get('/validate', async (req, res) => {
+app.get("/validate", async (req, res) => {
   try {
     if (req.headers.authorization) {
-      const user = await getCurrentUser(req.headers.authorization)
+      const user = await getCurrentUser(req.headers.authorization);
       // @ts-ignore
-      res.send({ user, token: getToken(user.id) })
+      res.send({ user, token: getToken(user.id) });
     }
   } catch (error) {
     // @ts-ignore
-    res.status(400).send({ error: error.message })
+    res.status(400).send({ error: error.message });
   }
-})
+});
 
-
-
+//creating a new user account
 app.post("/sign-up", async (req, res) => {
   const { email, password } = req.body;
 
@@ -110,7 +104,6 @@ app.post("/sign-up", async (req, res) => {
     }
 
     const user = await prisma.user.create({
-
       data: {
         email,
         password: bcrypt.hashSync(password),
@@ -127,8 +120,7 @@ app.post("/sign-up", async (req, res) => {
   }
 });
 
-
-
+//Giving accsess to the user into his account
 app.post("/sign-in", async (req, res) => {
   try {
     const email = req.body.email;
@@ -150,7 +142,6 @@ app.post("/sign-in", async (req, res) => {
     }
 
     const user = await prisma.user.findUnique({
-    
       where: { email },
     });
     if (user && verify(password, user.password)) {
@@ -162,6 +153,105 @@ app.post("/sign-in", async (req, res) => {
   } catch (error) {
     // @ts-ignore
     res.status(400).send({ errors: [error.message] });
+  }
+});
+
+//Getting all the messages
+app.get("/messages", async (req, res) => {
+  try {
+    const messages = await prisma.message.findMany({
+      include: { room: true, sender: true },
+    });
+    res.send(messages);
+  } catch (error) {
+    //@ts-ignore
+    res.status(400).send({ error: error.message });
+  }
+});
+
+//Creating a new message
+app.post("/messages", async (req, res) => {
+  try {
+    const roomId = Number(req.body.roomId);
+    const participantId = Number(req.body.participantId);
+    const newMessage = await prisma.message.create({
+      data: {
+        content: req.body.content,
+        room: {
+          connectOrCreate: {
+            where: { id: roomId },
+            create: { participants: { connect: { id: participantId } }, User: {connect: {email: req.body.email}} },
+          },
+        },
+        sender: { connect: { email: req.body.email } },
+      },
+    });
+    res.send(newMessage);
+  } catch (error) {
+    // @ts-ignore
+    res.status(400).send({ errors: [error.message] });
+  }
+});
+
+//Deleting a message
+app.delete("/messages/:id", async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const deletedMessage = await prisma.message.delete({ where: { id } });
+    if (deletedMessage) {
+      res.send({ message: "Message deleted" });
+    } else {
+      res.status(404).send({ error: "Message not found" });
+    }
+  } catch (error) {
+    //@ts-ignore
+    res.status(400).send({ error: error.message });
+  }
+});
+
+//creating a conversation
+app.post("/rooms", async (req, res) => {
+  try {
+    const messageId = Number(req.body.roomId);
+    const participantId = Number(req.body.participantId);
+
+    const room = await prisma.room.create({
+      data: {
+        User: { connect: { email: req.body.email } },
+        messages: {
+          connectOrCreate: {
+            where: { id: messageId },
+            create: {
+              content: req.body.content,
+              sender: { connect: { email: req.body.email } },
+            },
+          },
+        },
+        participants: { connect: { id: participantId } },
+      },
+    });
+    res.send(room);
+  } catch (error) {
+    // @ts-ignore
+    res.status(400).send({ errors: [error.message] });
+  }
+});
+
+
+
+//Delete a full conversation
+app.delete("/rooms/:id", async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const deletedRoom = await prisma.room.delete({ where: { id } });
+    if (deletedRoom) {
+      res.send({ message: "Conversation deleted" });
+    } else {
+      res.status(404).send({ error: "Conversation not found" });
+    }
+  } catch (error) {
+    //@ts-ignore
+    res.status(400).send({ error: error.message });
   }
 });
 
